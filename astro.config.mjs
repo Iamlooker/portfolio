d9e1b9e1e6ae28b00966a5c7a61ed68a1676d25a
LooKeR <iamlooker@proton.me>
2025-10-22 20:59:16 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iamlooker.github.io',
+  site: 'https://port.looker.ch',
   vite: {
     plugins: [tailwindcss()],
   },
